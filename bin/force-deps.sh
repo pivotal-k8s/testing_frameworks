0c5637f5115170fd3f5b7544a1cafdb8c6b1f197
Hannes Hörl <hhorl@pivotal.io>
2018-02-06 09:21:19 +0000

    Force all dependencies on k8s.io/...
    ... and print log versions of some interesting packages.

--- bin/force-deps.sh
+++ bin/force-deps.sh
@@ -12,11 +12,9 @@ BASE_DIR="$( cd "$(dirname "$0")/.." && pwd )"
 PATH="${PATH}:${GOPATH}/bin"
 
 ##------------------------------------------------------------
-forceClientGoVersion() {
-  local package="k8s.io/client-go"
-  local version="${1:-}"
-
-  rm -rf "${BASE_DIR}/vendor/${package}"
+forceVersion() {
+  local package="$1"
+  local version="$2"
 
   go get "${package}/..."
   (
@@ -27,14 +25,47 @@ forceClientGoVersion() {
 }
 
 ##------------------------------------------------------------
+deleteFromVendor() {
+  for d in "$@"
+  do
+    find "${BASE_DIR}/vendor" \
+      -wholename "*/${d}" \
+      -type d \
+      -exec rm -rf '{}' \+
+  done
+}
+
+##------------------------------------------------------------
+printVersion() {
+  local getVerScript='
+echo "found in $1:"
+cd "$1"
+git log -1 --no-color --format="%H, %aI, %d"
+'
+  for p in "$@"
+  do
+    find "${GOPATH}/src" \
+      -wholename "*/${p}" \
+      -type d \
+      -exec sh -c "$getVerScript" -- '{}' \;
+  done
+}
+
+##------------------------------------------------------------
 main() {
-  [ -z "${CLIENT_GO_VERSION:-}" ] && {
-    echo 'Using client-go from ./vendor' >&2
-    exit
+  [ -z "${CLIENT_GO_VERSION:-}" ] || {
+    {
+      echo "Setting client-go version to ${CLIENT_GO_VERSION}"
+      echo
+    }>&2
+
+    # deleteFromVendor 'k8s.io/client-go' 'k8s.io/api' 'k8s.io/apimachinery'
+    deleteFromVendor 'k8s.io'
+
+    forceVersion 'k8s.io/client-go' "$CLIENT_GO_VERSION"
   }
 
-  echo "Setting client-go version to ${CLIENT_GO_VERSION}" >&2
-  forceClientGoVersion "$CLIENT_GO_VERSION"
+  printVersion 'k8s.io/client-go' 'k8s.io/api' 'k8s.io/apimachinery'
 }
 
 ##------------------------------------------------------------
