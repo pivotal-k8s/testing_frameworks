0abeb68326b88f2b2e0a5cd3391347cda5648541
Hannes Hörl <hhorl@pivotal.io>
2018-02-12 12:23:00 +0000

    Add kube-{controller-manager,scheduler} binaries

--- integration/scripts/download-binaries.sh
+++ integration/scripts/download-binaries.sh
@@ -26,23 +26,33 @@ arch="$(uname -m)"
 
 dest_dir="${1:-"${test_framework_dir}/assets/bin"}"
 etcd_dest="${dest_dir}/etcd"
-kubectl_dest="${dest_dir}/kubectl"
 kube_apiserver_dest="${dest_dir}/kube-apiserver"
+kube_controller_manager_dest="${dest_dir}/kube-controller-manager"
+kube_scheduler_dest="${dest_dir}/kube-scheduler"
+kubectl_dest="${dest_dir}/kubectl"
 
 echo "About to download a couple of binaries. This might take a while..."
 
 curl $quiet "${BASE_URL}/etcd-${os}-${arch}" --output "$etcd_dest"
 curl $quiet "${BASE_URL}/kube-apiserver-${os}-${arch}" --output "$kube_apiserver_dest"
+curl $quiet "${BASE_URL}/kube-controller-manager-${os}-${arch}" --output "$kube_controller_manager_dest"
+curl $quiet "${BASE_URL}/kube-scheduler-${os}-${arch}" --output "$kube_scheduler_dest"
 
 kubectl_version="$(curl $quiet https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
 kubectl_url="https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/${os_lowercase}/amd64/kubectl"
 curl $quiet "$kubectl_url" --output "$kubectl_dest"
 
-chmod +x "$etcd_dest" "$kubectl_dest" "$kube_apiserver_dest"
+chmod +x \
+  "$etcd_dest" \
+  "$kube_apiserver_dest" \
+  "$kube_controller_manager_dest" \
+  "$kube_scheduler_dest" \
+  "$kubectl_dest"
 
-echo    "# destination:"
 echo    "#   ${dest_dir}"
 echo    "# versions:"
-echo -n "#   etcd:            "; "$etcd_dest" --version | head -n 1
-echo -n "#   kube-apiserver:  "; "$kube_apiserver_dest" --version
-echo -n "#   kubectl:         "; "$kubectl_dest" version --client --short
+echo -n "#   etcd:                    "; "$etcd_dest" --version | head -n 1
+echo -n "#   kube-apiserver:          "; "$kube_apiserver_dest" --version
+echo -n "#   kube-controller-manager: "; "$kube_controller_manager_dest" --version
+echo -n "#   kube-scheduler:          "; "$kube_scheduler_dest" --version
+echo -n "#   kubectl:                 "; "$kubectl_dest" version --client --short
