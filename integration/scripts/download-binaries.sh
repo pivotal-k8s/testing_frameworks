103a848cd0e73eeef151957da11fb247e8835029
Hannes Hörl <hhorl@pivotal.io>
2018-02-15 13:19:17 +0000

    Have the download script fetch the virtual-kubelet

--- integration/scripts/download-binaries.sh
+++ integration/scripts/download-binaries.sh
@@ -29,6 +29,7 @@ etcd_dest="${dest_dir}/etcd"
 kube_apiserver_dest="${dest_dir}/kube-apiserver"
 kube_controller_manager_dest="${dest_dir}/kube-controller-manager"
 kube_scheduler_dest="${dest_dir}/kube-scheduler"
+virtual_kubelet_dest="${dest_dir}/virtual-kubelet"
 kubectl_dest="${dest_dir}/kubectl"
 
 echo "About to download a couple of binaries. This might take a while..."
@@ -37,6 +38,7 @@ curl $quiet "${BASE_URL}/etcd-${os}-${arch}" --output "$etcd_dest"
 curl $quiet "${BASE_URL}/kube-apiserver-${os}-${arch}" --output "$kube_apiserver_dest"
 curl $quiet "${BASE_URL}/kube-controller-manager-${os}-${arch}" --output "$kube_controller_manager_dest"
 curl $quiet "${BASE_URL}/kube-scheduler-${os}-${arch}" --output "$kube_scheduler_dest"
+curl $quiet "${BASE_URL}/virtual-kubelet-${os}-${arch}" --output "$virtual_kubelet_dest"
 
 kubectl_version="$(curl $quiet https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
 kubectl_url="https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/${os_lowercase}/amd64/kubectl"
@@ -47,6 +49,7 @@ chmod +x \
   "$kube_apiserver_dest" \
   "$kube_controller_manager_dest" \
   "$kube_scheduler_dest" \
+  "$virtual_kubelet_dest" \
   "$kubectl_dest"
 
 echo    "#   ${dest_dir}"
@@ -55,4 +58,5 @@ echo -n "#   etcd:                    "; "$etcd_dest" --version | head -n 1
 echo -n "#   kube-apiserver:          "; "$kube_apiserver_dest" --version
 echo -n "#   kube-controller-manager: "; "$kube_controller_manager_dest" --version
 echo -n "#   kube-scheduler:          "; "$kube_scheduler_dest" --version
+echo -n "#   virtual-kubelet:         "; "$virtual_kubelet_dest" --provider=doesNotMatter version
 echo -n "#   kubectl:                 "; "$kubectl_dest" version --client --short
